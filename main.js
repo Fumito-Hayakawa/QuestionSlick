77af128c14ebaaa9a35e8e300e91f2ff26af1020
EC2 Default User <ec2-user@ip-172-31-49-188.ec2.internal>
2021-03-10 03:10:21 +0000

    slickご質問

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+$('.slider').slick({
+  accessibility: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  arrows: true,
+  prevArrow: '<img src="http://kasumiblog.xsrv.jp/codepen/images/arrow_lft.png" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="http://kasumiblog.xsrv.jp/codepen/images/arrow_rig.png" class="slide-arrow next-arrow">',
+});
+
+})
+
